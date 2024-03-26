0395ffc40341e20e51d065b0d3366dd6c7c80e25
yzb <zhongbao.yue@holla.world>
2024-03-26 19:19:11 +0800

    feat: vue自定义组件&组件props传值&事件回调

---
+++ src/App.vue
@@ -0,0 +1,126 @@
+<script setup>
+import { ref, reactive, computed,watch, onMounted,watchEffect,watchPostEffect,onUpdated, onBeforeUpdate,onBeforeMount } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+import Count from './components/Count.vue'
+
+const state = ref({
+  count: 0
+})
+
+watchPostEffect(() => {
+  /* 在 Vue 更新后执行 */
+  console.error('watchPostEffect',state.value.count)
+})
+watch([()=> state.value.count], (count)=>{
+  console.error('watch immediate: true')
+},{immediate: true})
+
+watchEffect(() => {
+  console.error('watchEffect',state.value.count)
+})
+
+watch([()=> state.value.count], (count)=>{
+  console.error('watch immediate: true')
+},{immediate: true})
+
+onBeforeMount(() => {
+  console.error('beforeMount')
+})
+
+onMounted(() => {
+  console.error('mounted')
+})
+
+onBeforeUpdate(() => {
+  console.error('beforeUpdate')
+})
+
+onUpdated(() => {
+  console.error('updated')
+})
+
+const incrementCb = (count,a,b) => {
+  console.error('incrementCb',count)
+  console.error('incrementCb',a)
+  console.error('incrementCb',b)
+}
+
+
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg= "You did it!"/>
+    </div>
+
+    <Count :initialCount="999" @incrementCb="incrementCb"/>
+  </header>
+
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+}
+</style>
---
+++ src/components/Count.vue
@@ -0,0 +1,26 @@
+<script setup>
+import {ref} from 'vue'
+
+const props = defineProps({
+    initialCount: {
+        type: Number,
+        default: 0
+    }
+})
+const emit = defineEmits({
+    incrementCb: null
+})
+
+const count = ref(props.initialCount)
+const increment = () => {
+    count.value++
+    emit('incrementCb', count.value, 1, 2)
+}
+</script>
+
+<template>
+    <div>
+        <h1>Count: {{ count }}</h1>
+        <button @click="increment">Increment</button>
+    </div>
+</template>
