8543f8bbab2b7524cfe91c8376b834c741da61c1
yuezhongbao <yzb99666@163.com>
2024-05-08 10:26:00 +0800

    feat: 联调socket

--- src/views/VideoStreamView.vue
+++ src/views/VideoStreamView.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <h1>1 v 1</h1>
+    <button @click="startVideoCall">邀请好友</button>
     <div class="video-container">
       <div class="video-player">
         <video ref="localPlayer" autoplay></video>
@@ -11,29 +12,62 @@
     </div>
   </div>
 </template>
-
 <script>
+import ws from '../ws.client'
 export default {
   name: 'VideoStreamView',
   data() {
     return {
       peerConnection: null,
+      localStream: null
     }
   },
-  mounted() {
-   this.startVideoCall()
+  async mounted() {
+    this.getLocalStream()
+    let roomId = this.$route.query.roomId
+    if(!roomId) {
+      roomId = Math.random().toString(36).substring(7)
+      this.$router.replace({query: {roomId}})
+    } 
+    ws.emit('join', roomId)
+
+    const peerConnection = await this.createPeerConnection()
+    ws.on('message', async (message) => {
+      console.error('收到消息', message)
+      switch(message.type) {
+        case 'join': 
+          console.error('接到join通知')
+          const offer = await peerConnection.createOffer()
+          await peerConnection.setLocalDescription(offer)
+          ws.emit('message', message.payload.socketId, {type: 'offer', payload: {offer}})
+          break
+        case 'offer':
+          console.error('接到offer通知')
+          const sdpOffer = new RTCSessionDescription(message.payload.offer)
+          await peerConnection.setRemoteDescription(sdpOffer)
+          const answer = await peerConnection.createAnswer()
+          await peerConnection.setLocalDescription(answer)
+          ws.emit('message', message.payload.socketId, {type: 'answer', payload: {answer}})
+          break
+        case 'answer':
+          console.error('接到answer通知')
+          const sdpAnswer = new RTCSessionDescription(message.payload.answer)
+          await peerConnection.setRemoteDescription(sdpAnswer)
+          break
+      }
+    })
   },
   methods: {
     async getLocalStream () {
+      if(this.localStream) return  this.localStream
       const stream = await navigator.mediaDevices.getUserMedia({ video: true })
       this.$refs.localPlayer.srcObject = stream
-      // this.$refs.remotePlayer.srcObject = stream
+
+      this.localStream = stream
       return stream
     },  
     // 创建对等链接
     async createPeerConnection() {
-      if(this.peerConnection) return this.peerConnection
-
       const configuration = {
         iceServers: [
           {urls: 'stun:stun.l.google.com:19302'}, 
@@ -56,48 +90,36 @@ export default {
       peerConnection.onicecandidate = event => {
         if (event.candidate) {
           // 1.将 ICE 候选发送给 对方
-          // 示例：ws.send('sendICECandidate', event.candidate)
+          // ws.send('sendICECandidate', event.candidate)
           // 2.同时监听对方 ICE 候选
           // 示例：ws.on('receiveICECandidate', (candidate) => this.receiveICECandidate(candidate))
         }
       }
-      this.peerConnection = peerConnection
-
       return peerConnection
     },
     async startVideoCall() {
-      const peerConnection = await this.createPeerConnection()
-
-      const shareCode = this.$route.query.shareCode; // 获取 URL 参数中的 shareCode
-      if(shareCode) {
-        // 如果是被邀请者
-        // 1. 监听ws 接收SDP offer
-        // 示例：ws.on('receiveSDPOffer', (sdpOffer) => this.receiveSDPOffer(sdpOffer))
-        // 2. 创建 answer
-        const answer = await peerConnection.createAnswer()
-        await peerConnection.setLocalDescription(answer)
-        // 3. 发送 answer
-        // 示例：ws.send('sendSDPAnswer', answer)
-        
-      }else {
-        // 如果是邀请者
-        // 1. 监听ws 接收SDP answer
-        // 示例：ws.on('receiveSDPAnswer', (sdpAnswer) => this.receiveSDPAnswer(sdpAnswer))
-        // 2. 创建 offer
-        const offer = await peerConnection.createOffer()
-        await peerConnection.setLocalDescription(offer)
-        // 3. 发送 offer
-        // 示例：ws.send('sendSDPOffer', offer)
-      }
+      const url = window.location.href
+      navigator.clipboard.writeText(url)
+      .then(() => {
+        alert('分享链接已复制到剪贴板,快去发给好友试试吧!')
+      })
+      .catch((error) => {
+        alert('无法复制到剪贴板,请你手动复制吧!')
+      })
     },
     async receiveSDPAnswer(sdpAnswer) {
-      await peerConnection.setRemoteDescription(sdpAnswer)
+      console.error('1p: 我收到answer了!', sdpAnswer)
+      await this.peerConnection.setRemoteDescription(sdpAnswer)
     },
-    async receiveSDPOffer(sdpOffer) {
-      await peerConnection.setRemoteDescription(sdpOffer)
+    async receiveSDPOffer(params) {
+      console.error('2p: 我收到offer了!', params)
+      await this.peerConnection.setRemoteDescription(params.offer)
+      const answer = await this.peerConnection.createAnswer()
+      await this.peerConnection.setLocalDescription(answer)
+      ws.emit('sendSDPAnswer', answer)
     },
     async receiveICECandidate(candidate) {
-      await peerConnection.addIceCandidate(new RTCIceCandidate(candidate))
+      await this.peerConnection.addIceCandidate(new RTCIceCandidate(candidate))
     }
   }
 
